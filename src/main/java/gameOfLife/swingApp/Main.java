93e884656d3efad1c9db20ca751d08e2a0bedbcc
Alena Zahradníčková <xzahrad4@fi.muni.cz>
2019-02-28 21:11:19 +0100

    rename Frame -> MainPanel, repainting old game, addKeyListener for enter, pom updated (executable swing app)

--- src/main/java/gameOfLife/swingApp/Main.java
+++ src/main/java/gameOfLife/swingApp/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Game of Life");
-        frame.setContentPane(new Frame().getMainPanel());
+        frame.setContentPane(new MainPanel().getPanel());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
