29faff71251ee1b46fcbf4444f3982e23a801ffa
Alena Zahradníčková <xzahrad4@fi.muni.cz>
2019-03-01 09:56:35 +0100

    imports, frame size

--- src/main/java/gameOfLife/swingApp/Main.java
+++ src/main/java/gameOfLife/swingApp/Main.java
@@ -1,12 +1,18 @@
 package gameOfLife.swingApp;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.*;
 
 public class Main {
+    private static final int HEIGHT = 500;
+    private static final int WIDTH = 800;
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Game of Life");
         frame.setContentPane(new MainPanel().getPanel());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         frame.pack();
         frame.setVisible(true);
     }
